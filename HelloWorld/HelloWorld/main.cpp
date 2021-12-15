67c02c9c96ead1d3f91a3fb8f0d521541ffbe248
gauravbjain <gauravbjain@gmail.com>
2021-12-15 20:27:58 +0530

    changes

--- HelloWorld/HelloWorld/main.cpp
+++ HelloWorld/HelloWorld/main.cpp
@@ -9,7 +9,7 @@
 #include <vector>
 
 template<typename T>
-void PrintVector(std::vector<T> const & InVec){
+void PrintVector(std::vector<T> const & InVec) {
     for (auto itr: InVec)
         std::cout<< std::endl<< itr << " ";
     std::cout << std::endl;
@@ -17,9 +17,10 @@ void PrintVector(std::vector<T> const & InVec){
 }
 
 int main(int argc, const char * argv[]) {
+
     // insert code here...
     std::cout << "Hello, World!\n";
-    std::vector<int> numbers = {10,192,19,12};
+    std::vector<int> numbers = {2, 10,192,19,12};
     PrintVector(numbers);
     std::vector<int> testNumb(5,0);
     PrintVector(testNumb);
